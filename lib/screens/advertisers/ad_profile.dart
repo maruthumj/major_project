b4518f3dfd69f9e44fd129badf5ff95055941afa
maruthumj <maruthupandian72@gmail.com>
2021-03-05 11:01:11 +0530

    added profile page

---
+++ lib/screens/advertisers/ad_profile.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ad_profile extends StatefulWidget {
+  ad_profile({Key key}) : super(key: key);
+
+  @override
+  _ad_profileState createState() => _ad_profileState();
+}
+
+class _ad_profileState extends State<ad_profile> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: SingleChildScrollView(
+        child: Stack(
+          children: [
+            Column(
+              children: [],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
